dcc1678df9cb9b2b19db45ab682d966d9e2c0c41
Rian <rian.sj@gmail.com>
2019-02-17 15:21:31 +0700

    add Filtered-LS.js

--- Filtered-LS.js
+++ Filtered-LS.js
@@ -1,3 +1,15 @@
 const fs = require('fs');
 
-console.log(__dirname);
+const dir = process.argv[2];
+const ext = '.' + process.argv[3];
+
+if (dir) {
+    fs.readdir(dir, (err, files) => {
+        if (err) return console.log(err);
+        //const filteredFiles = files.filter( file => file.indexOf(ext) !== -1 );
+        //console.log(filteredFiles.join('\n'));
+        files.forEach( file => {
+            if (file.indexOf(ext) !== -1) console.log(file);
+        });
+    });
+}
