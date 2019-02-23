18a027b56ec56fc4c5e8814c9fba782bf932a2af
Rian <rian.sj@gmail.com>
2019-02-23 22:27:20 +0700

    add httpfile server and httpuppercase

--- HttpFileServer.js
+++ HttpFileServer.js
@@ -1,10 +1,11 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = process.argv[2];
 const filePath = process.argv[3];
 
 const server = http.createServer( (req, res) => {
-    res.end('Hello from server.');
+    fs.createReadStream(filePath).pipe(res);    
 });
 
 server.listen(port, (err) => {
